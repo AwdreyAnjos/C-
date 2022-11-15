b0ac3f2584c7d227c8407a63e9072daa8a9df20f
AwdreyAnjos <awdrey.Anjos@gmail.com>
2022-11-15 02:24:39 +0000

    Começando Curso C#Collections - Criando Arrays

---
+++ CSharpCollections/CsharpCollections/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aulaIntro = "Introduções às Coleções";
+            string aulaModelando = "Modelando a Classe Aula";
+            string aulaSets = "Trabalhando com Conjuntos";
+
+            //string[] aulas = new string[]
+            //{
+            //   aulaIntro,
+            //   aulaModelando,
+            //   aulaSets
+            //};
+
+            string[] aulas = new string[3];
+            aulas[0] = aulaIntro;
+            aulas[1] = aulaModelando;
+            aulas[2] = aulaSets;
+
+            NewMethod(aulas);
+
+            aulas[0] = "Trabalhando com Arrays"; 
+        }
+
+        private static void NewMethod(string[] aulas)
+        {
+            //foreach (var aula in aulas)
+            //{
+            //    Console.WriteLine(aula);
+            //}
+            for (int i = 0; i < aulas.Length; i++)
+            {
+                Console.WriteLine(aulas[i]);
+            }
+        }
+    }
+}
